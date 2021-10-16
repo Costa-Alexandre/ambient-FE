44170fa9c7f612733609ad70aabcfcf7dd9865b8
Alexandre Costa <alexandre.costa@code.berlin>
2021-10-17 01:10:33 +0200

    Menu.js component

--- screens/App/Home.js
+++ screens/App/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Menu from '../../components/menu/Menu';
 
 export default function Home() {
 
     return (
       <View style={styles.container}>
+        <Menu />
         <Text style={{color: 'white'}}>Logged</Text>
       </View>
     );
