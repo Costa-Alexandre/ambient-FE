de4a8be5f53182814bb80caee7cb654e08b3c81a
Alexandre Costa <alexandre.costa@code.berlin>
2021-10-17 17:07:25 +0200

    set icons from tab navigation

--- routes/tabNavigator.js
+++ routes/tabNavigator.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from '../screens/App/Home';
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import CustomIcon from '../components/icons/CustomIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? ''
+                : '';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? '' : '';
+            }
+
+            // You can return any component that you like here!
+            return <CustomIcon name={route.name} size={20} color='#000' />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="home" component={Home} />
+        <Tab.Screen name="clips" component={Home} />
+        <Tab.Screen name="search" component={Home} />
+        <Tab.Screen name="message" component={Home} />
       </Tab.Navigator>
   );
 }
