9403c6d46c0910518477a8357908c28ce5d3e817
Joshua Knauber <joshua.knauber@gmail.com>
2021-10-30 13:19:11 +0200

    switched to using local ip adress

--- api/users.js
+++ api/users.js
@@ -1,33 +1,17 @@
 import axios from 'react-native-axios';
 
 
+const baseUrl = "http://192.168.178.22:3000" // replace this ip adress with your local ip or the deployed servers address
+
+
 export const signInUser = (userData) => {
-    const data = { token: "test", firstName: userData.display_name, lastName: userData.id, email: userData.email, avatar: "" }
-    console.log(data)
-    const header = {
-        method: 'POST',
-        url: 'http://127.0.0.1:3000/api/user',
-        data: JSON.stringify(data),
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-        }
-    }
-    axios(header)
+    const key = "test3"
+    const data = { token: key, firstName: key, lastName: key, username: key, email: key, avatar: key }
+
+    axios.post(`${baseUrl}/api/user`, data)
     .then(result => {
         console.log(result)
     })
     .catch(error => console.log(error))
 
-    // fetch("http://127.0.0.1:3000/api/user",
-    // {
-    //     headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json'
-    //     },
-    //     method: "POST",
-    //     body: JSON.stringify(data)
-    // })
-    // .then(function(res){ console.log(res) })
-    // .catch(function(res){ console.log(res) })
 }
