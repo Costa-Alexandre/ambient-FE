f2bc911ba8aafbbf28d1c5a7cb9f73d0dda10fad
Alexandre Costa <alexandre.costa@code.berlin>
2021-12-05 05:14:41 +0100

    this merges joshua's commit for averageColor

---
+++ src/hooks/averageColor.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import ImageColors from 'react-native-image-colors'
+
+
+
+const useAverageColor = (defaultUri, defaultBg) => {
+
+  const [imageUri, setImageUri] = useState(defaultUri)
+  const [bgColor, setBgColor] = useState(defaultBg)
+
+  const updateBgColor = async (uri) => {
+    if (uri) {
+      const result = await ImageColors.getColors(uri, {
+        fallback: defaultBg,
+        cache: true,
+      })
+
+      if (result.platform === "android") {
+        setBgColor(result.average)
+      } else {
+        throw new Error('Unexpected platform key')
+      }
+    } else {
+      setBgColor(defaultBg)
+    }
+  }
+
+  useEffect(() => {
+    updateBgColor(imageUri)
+  }, [imageUri])
+
+  return [bgColor, setImageUri];
+}
+
+export default useAverageColor; 
--- src/ui/PlayingSong.js
+++ src/ui/PlayingSong.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { colorStyles } from "styles";
+import useAverageColor from 'hooks/averageColor';
+import { MainContext } from "store/MainProvider";
 
 export default function PlayingSong({ children, callback=null }) {
+  const { activeTrack } = useContext(MainContext);
+  const [averageColor, setImageUri] = useAverageColor(activeTrack.imageUri, "#1B1B1F")
+
+  useEffect(() => {
+    setImageUri(activeTrack.imageUri)
+  }, [activeTrack])
 
   return (
     <TouchableOpacity
-      style={[styles.outerContainer, { backgroundColor: colorStyles.buttonSolid }]}
+      style={[styles.outerContainer, { backgroundColor: averageColor }]}
       onPress={callback}
     >
       {children}
