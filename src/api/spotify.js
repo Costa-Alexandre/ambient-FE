f1f47e29ab81b72beea543147147809176668497
= <=>
2021-12-16 12:34:51 +0100

    fix device playback by playing on active or first found smartphone

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -57,13 +57,38 @@ export const spotifyGetTrack = async (trackId, token = null) => {
   }
 };
 
+export const spotifyGetDeviceId = async () => {
+  return SpotifyAuth.getSession()
+  .then(session => session.accessToken)
+  .then(token => fetch( `${spotifyBaseUrl}/me/player/devices`, spotifyRequestOptions(token) ))
+  .then(data => data.json())
+  .then(devices => devices.devices)
+  .catch(error => {console.log(error)})
+}
+
+export const spotifyGetActiveDevice = async () => {
+  return spotifyGetDeviceId()
+  .then(devices => {
+      let device = devices.filter(device => device.is_active)
+      if (device.length === 1) {return device[0]}
+      device = devices.filter(device => device.type === "Smartphone")
+      if (device.length > 0) {return device[0]}
+      return null
+  .catch(error => {console.log(error)})
+  })
+}
+
 export const spotifyPlayTrack = async (trackUri, position=0) => {
   try {
     const session = await SpotifyAuth.getSession();
     token = session.accessToken;
 
+    let device = await spotifyGetActiveDevice()
+    let device_id = device ? `?device_id=${device.id}` : ""
+    console.log("d", device)
+
     await fetch(
-      `${spotifyBaseUrl}/me/player/play`,
+      `${spotifyBaseUrl}/me/player/play` + device_id,
       {
         method: 'PUT',
         headers: {
