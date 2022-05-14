9167a577a7e491894c139bf40b99a2f9787d5007
Alexandre Costa <alexandre.costa@code.berlin>
2022-05-14 18:58:45 +0200

    fix: pass valid show parameters for creating show

--- src/screens/home/components/CreateShowModalize.js
+++ src/screens/home/components/CreateShowModalize.js
@@ -1,79 +1,97 @@
-import React, {useRef, useState, useEffect } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import { Modalize } from "react-native-modalize";
+import React, { useRef, useState, useEffect, useContext } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Modalize } from 'react-native-modalize';
 import { useModalize } from 'react-native-modalize/lib/utils/use-modalize';
-import { CustomButton } from "ui";
-import { fontStyles, colorStyles } from "styles";
-import { postShow } from "api/shows";
+import { CustomButton } from 'ui';
+import { MainContext } from 'store/MainProvider';
+import { fontStyles, colorStyles } from 'styles';
+import { postShow } from 'api/shows';
 
-export default function CreateShowModalize({ openModal, onClose, callback=null }) {
-
+export default function CreateShowModalize({
+  openModal,
+  onClose,
+  callback = null,
+}) {
   const { ref, open, close } = useModalize();
-  const [showName, setShowName] = useState("");
-  const [showDescription, setShowDescription] = useState("");
+  const [showName, setShowName] = useState('');
+  const [showDescription, setShowDescription] = useState('');
+  const { user } = useContext(MainContext);
 
   useEffect(() => {
     openModal && open();
-  }, [])
+  }, []);
 
   const createShow = async () => {
     const newShow = await postShow({
       name: showName,
-      description: showDescription
+      description: showDescription,
+      creator_id: JSON.stringify(user._id).replace(/\"/g, ''),
+      date_created: JSON.stringify(new Date()).replace(/\"/g, ''),
+      date_scheduled: JSON.stringify(new Date()).replace(/\"/g, ''),
     });
     close();
     callback(newShow);
-  }
-
+  };
 
   return (
-      <Modalize
-          ref={ref}
-          handlePosition={"inside"}
-          handleStyle={{width: 44, height: 6, borderRadius: 14, backgroundColor: colorStyles.textSecondary}}
-          onClose={onClose}
-          modalHeight={353}
-          withOverlay={true}
-          modalStyle={styles.rootModalize}
-        >
-        <View style={styles.content}>
-          <View style={styles.inputContainer}>
-            <View style={styles.frame}>
-              <Text style={composeText}>Show Name</Text>
-              <TextInput 
-                onChangeText={(showName) => setShowName(showName)}
-                value={showName}
-                style={[composeInput, {height: 46}]}
-                placeholder="Name your show..."
-                placeholderTextColor="#ffffff50"
-              />
-            </View>
-            <View style={styles.frame}>
-              <Text style={composeText}>Description</Text>
-              <TextInput 
-                onChangeText={(showDescription) => setShowDescription(showDescription)}
-                value={showDescription}
-                style={[composeInput, {height: 73}]}
-                placeholder="Quickly describe your shows content here..."
-                placeholderTextColor="#ffffff50"
-              />
-            </View>
+    <Modalize
+      ref={ref}
+      handlePosition={'inside'}
+      handleStyle={{
+        width: 44,
+        height: 6,
+        borderRadius: 14,
+        backgroundColor: colorStyles.textSecondary,
+      }}
+      onClose={onClose}
+      modalHeight={353}
+      withOverlay={true}
+      modalStyle={styles.rootModalize}
+    >
+      <View style={styles.content}>
+        <View style={styles.inputContainer}>
+          <View style={styles.frame}>
+            <Text style={composeText}>Show Name</Text>
+            <TextInput
+              onChangeText={(showName) => setShowName(showName)}
+              value={showName}
+              style={[composeInput, { height: 46 }]}
+              placeholder="Name your show..."
+              placeholderTextColor="#ffffff50"
+            />
           </View>
-          <View style={styles.buttonContainer}>
-            <CustomButton size="startShowButton" color="button" title="Schedule" callback={null} />
-            <View style={{width: 16}}></View>
-            <CustomButton 
-              size="startShowButton" 
-              color="accent" 
-              title="Start Show" 
-              callback={() => createShow(
-              )} />
-            </View>
+          <View style={styles.frame}>
+            <Text style={composeText}>Description</Text>
+            <TextInput
+              onChangeText={(showDescription) =>
+                setShowDescription(showDescription)
+              }
+              value={showDescription}
+              style={[composeInput, { height: 73 }]}
+              placeholder="Quickly describe your shows content here..."
+              placeholderTextColor="#ffffff50"
+            />
+          </View>
         </View>
-      </Modalize>
-  )
+        <View style={styles.buttonContainer}>
+          <CustomButton
+            size="startShowButton"
+            color="button"
+            title="Schedule"
+            callback={null}
+          />
+          <View style={{ width: 16 }}></View>
+          <CustomButton
+            size="startShowButton"
+            color="accent"
+            title="Start Show"
+            callback={() => createShow()}
+          />
+        </View>
+      </View>
+    </Modalize>
+  );
 }
-
 
 const styles = StyleSheet.create({
   rootModalize: {
@@ -89,18 +107,18 @@ const styles = StyleSheet.create({
     marginBottom: 32,
   },
   buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   inputContainer: {
-    flexDirection: "column",
+    flexDirection: 'column',
   },
   frame: {
-    flexDirection: "column",
+    flexDirection: 'column',
     marginBottom: 32,
   },
   input: {
-    width: "100%",
+    width: '100%',
     backgroundColor: colorStyles.input,
     borderRadius: 8,
     color: colorStyles.text,
